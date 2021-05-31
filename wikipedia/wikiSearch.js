bfbc5e645b7e78e103d7a35b684cf871ef0cab99
yasserhcn <hacini_yasser@yahoo.fr>
2021-05-31 17:06:18 +0100

    moved wikisearch to a folder

--- wikipedia/wikiSearch.js
+++ wikipedia/wikiSearch.js
@@ -1,6 +1,7 @@
 const request = require('request');
 const discord = require('discord.js');
-function wikiSearch(msg, keyword)
+
+function wikiSearch(msg, keyword, index)
 {
     let searchIndex = 69;
 
@@ -21,19 +22,25 @@ function wikiSearch(msg, keyword)
     let links = JSON.parse(' [ "something" , "failed" , "try again"] ');
     
     request(url, function (error, response, body) {
-        console.error('error:', error); // Print the error if one occurred
+        if(error) { console.error('error:', error) }; // Print the error if one occurred
         parsedPage = JSON.parse(body);
         names = parsedPage[1];
         links = parsedPage[3];
         
         let message = "found these results in wikipedia:\n\n";
 
-        for (let i = 0; i < names.length; i++) {
-          message += names[i] + " <" + links[i] + ">";
-          message += "\n";
+        if(names.length == 0){
+          message = "no results found :(\n";
+        }else{
+          for (let i = 0; i < names.length; i++) {
+            message += names[i] + " <" + links[i] + ">";
+            message += "\n";
+          }
         }
-
-        parseWiki(links[searchIndex], names[searchIndex]);
+        
+        if(index){
+          parseWiki(links[searchIndex], names[searchIndex]);
+        }
         
         msg.channel.send(message);
       });
